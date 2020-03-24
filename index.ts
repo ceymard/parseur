aa0eca3ec8105e752489aba024b8aaccf5cd849f
Christophe Eymard <christophe.eymard@sales-way.com>
2020-03-24 23:49:06 +0100

    max parse result and many more.

--- index.ts
+++ index.ts
@@ -10,12 +10,21 @@ export class ParseResult<T> {
 
 
 export function escape(str: string) {
-  return str.replace(/[?.\[\]\(\)*+]/g, m => '\\' + m)
+  return str.replace(/[?.\[\]\(\)*+$]/g, m => '\\' + m)
 }
 
 
 export function Res<T>(res: T, pos: number) {
-  return new ParseResult(res, pos)
+  var r = new ParseResult(res, pos)
+  var max = Res.max_res
+  if (!max) Res.max_res = r
+  else if (max && max.pos -1 < r.pos)
+    Res.max_res = r
+  return r
+}
+
+export namespace Res {
+  export var max_res: ParseResult<any> | null = null
 }
 
 
@@ -23,13 +32,14 @@ export class Rule<T> {
 
   constructor(public parse: (input: string, pos: number, skip: RegExp | undefined) => TNoMatch | ParseResult<T>) { }
 
-  map<U>(fn: (res: T, input: string, pos: number, skip: RegExp | undefined, start: number) => U | TNoMatch | ParseResult<U>): Rule<U> {
+  map<U>(fn: (res: T, input: string, pos: number, skip: RegExp | undefined, start: number) => U | TNoMatch | ParseResult<U> | Rule<U>): Rule<U> {
     return new Rule((input, pos, skip) => {
       var res = this.parse(input, pos, skip)
       if (res === NoMatch) return NoMatch
       var res2 = fn(res.res, input, res.pos, skip, pos)
       if (res2 === NoMatch) return NoMatch
       if (res2 instanceof ParseResult) return res2
+      if (res2 instanceof Rule) return res2.parse(input, res.pos, skip)
       return Res(res2, res.pos)
     })
   }
@@ -40,6 +50,11 @@ export class Rule<T> {
       return res
     })
   }
+
+  Repeat() { return Repeat(this) }
+  Optional() { return Opt(this) }
+  OneOrMore() { return OneOrMore(this) }
+  SeparatedBy(rule: RawRule<any>): Rule<T[]> { return SeparatedBy(this, rule) }
 }
 
 
@@ -99,47 +114,44 @@ export function Reg(reg: RegExp): Rule<RegExpMatchArray> {
 /**
  *
  */
-export function _(exp: RegExp): Rule<RegExpMatchArray>
-export function _(exp: string): Rule<string>
-export function _<T>(exp: Rule<T>): Rule<T>
-export function _<T>(exp: string | RegExp | Rule<T>): Rule<string | RegExpMatchArray | T>
-export function _(exp: any) {
+export function R(exp: RegExp): Rule<RegExpMatchArray>
+export function R(exp: string): Rule<string>
+export function R<T>(exp: Rule<T>): Rule<T>
+export function R<T>(exp: string | RegExp | Rule<T>): Rule<string | RegExpMatchArray | T>
+export function R(exp: any) {
   if (exp instanceof Rule) return exp
   if (typeof exp === 'string') return Str(exp)
   return Reg(exp)
 }
 
 
-/**
- *
- */
-export function Seq<T extends {[name: string]: RawRule<any>}>(obj: T): Rule<{[K in keyof T]: Result<T[K]>}> {
-  var entries = Object.entries(obj)
-    .map(obj => [obj[0], _(obj[1])] as [keyof T, Rule<any>])
+export type UnionToIntersection<U> =
+  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
-  return new Rule(function (input, pos, skip) {
-    var res = {} as {[K in keyof T]: Result<T[K]>}
+export function Seq<T extends (RawRule<any> | {[name: string]: RawRule<any>})[]>(...seq: T): Rule<UnionToIntersection<
+  {[K in keyof T]: T[K] extends {[name: string]: RawRule<any>} ?
+    {[K2 in keyof T[K]]: Result<T[K][K2]>}
+    : never
+  }[number]
+>> {
+  var entries = [] as [null | string, Rule<any>][]
+  for (var _r of seq) {
+    if (_r instanceof RegExp || typeof _r === 'string' || _r instanceof Rule) {
+      entries.push([null, R(_r)])
+    } else {
+      // This is a named rule object.
+      entries.push(...Object.entries(_r).map(([key, _r]) => [key, R(_r)] as [string, Rule<any>]))
+    }
+  }
+
+  return new Rule<any>(function (input, pos, skip) {
+    var res = {} as any
     for (var i = 0, l = entries.length; i < l; i++) {
       var key = entries[i][0]
       var match = entries[i][1].parse(input, pos, skip)
       if (match === NoMatch) return NoMatch
       pos = match.pos
-      res[key] = match.res
-    }
-    return Res(res, pos)
-  })
-}
-
-export function SeqArray<T extends RawRule<any>[]>(..._rules: T): Rule<{[K in keyof T]: Result<T[K]>}> {
-  var rules = _rules.map(r => _(r))
-  return new Rule(function (input, pos, skip) {
-    var res = [] as unknown as {[K in keyof T]: Result<T[K]>}
-    for (var i = 0, l = rules.length; i < l; i++) {
-      const rule = rules[i]
-      const match = rule.parse(input, pos, skip)
-      if (match === NoMatch) return NoMatch
-      res.push(match.res)
-      pos = match.pos
+      if (key != null) res[key] = match.res
     }
     return Res(res, pos)
   })
@@ -150,7 +162,7 @@ export function SeqArray<T extends RawRule<any>[]>(..._rules: T): Rule<{[K in ke
  *
  */
 export function Either<T extends RawRule<any>[]>(...rules: T): Rule<{[K in keyof T]: Result<T[K]>}[number]> {
-  var _rules = rules.map(r => _(r))
+  var _rules = rules.map(r => R(r))
 
   return new Rule(function (input, pos, skip) {
     for (var i = 0, l = _rules.length; i < l; i++) {
@@ -177,7 +189,7 @@ export function OneOrMore<R extends RawRule<any>>(r: R) {
  *
  */
 export function Repeat<R extends RawRule<any>>(r: R): Rule<Result<R>[]> {
-  var rule = _(r)
+  var rule = R(r)
   return new Rule(function (input, pos, skip) {
     var res: Result<R>[] = []
     var rres: ParseResult<any> | TNoMatch
@@ -194,7 +206,7 @@ export function Repeat<R extends RawRule<any>>(r: R): Rule<Result<R>[]> {
  *
  */
 export function Opt<R extends RawRule<any>>(r: R): Rule<Result<R> | null> {
-  var rule = _(r)
+  var rule = R(r)
   return new Rule(function (input, pos, skip) {
     var res = rule.parse(input, pos, skip)
     if (res === NoMatch) return Res(null, pos)
@@ -207,7 +219,7 @@ export function Opt<R extends RawRule<any>>(r: R): Rule<Result<R> | null> {
  *
  */
 export function Not<R extends RawRule<any>>(r: R): Rule<null> {
-  var rule = _(r)
+  var rule = R(r)
   return new Rule(function (input, pos, skip) {
     var res = rule.parse(input, pos, skip)
     if (res === NoMatch) return Res(null, pos)
@@ -226,6 +238,7 @@ export const Any = new Rule((input, pos, skip) => {
     while ((skip.lastIndex = pos, mskip = skip.exec(input))) {
       pos += mskip[0].length
     }
+    if (pos >= input.length) return NoMatch
     var res = [] as string[]
     // now we've skipped, we're on a potential match
     while ((skip.lastIndex = pos, mskip = skip.exec(input), pos < input.length && mskip === null)) {
@@ -239,62 +252,36 @@ export const Any = new Rule((input, pos, skip) => {
 })
 
 
-export function Until(reg: RawRule<any>) {
-  var rule = _(reg)
-  return Repeat(
-    Seq({_1: Not(rule), char: Any}).map(r => r.char)
-  )
-}
-
-
-export function Parser<T>(rule: Rule<T>, skip?: RegExp) {
-  return function (input: string) {
-    return rule.parse(input, 0, skip ? new RegExp(skip.source, skip.flags + 'y') : undefined)
-  }
-}
-
-
-/**
- *
- */
-export function S<Rules extends RawRule<any>[]>(tpl: TemplateStringsArray, ...rules: Rules): Rule<Rules extends [] ? null : Rules extends [RawRule<infer T>] ? T : {[K in keyof Rules]: Result<Rules[K]>}> {
-  var match_rules = [] as Rule<any>[]
-  var indexes = [] as (number | null)[]
-
-  for (var i = 0, l = tpl.length; i < l; i++) {
-    var str = tpl[i].trim().split(/\s+/g)
-    for (var k = 0, lk = str.length; k < lk; k++) {
-      if (!str[k]) continue
-      indexes.push(null)
-      match_rules.push(Str(str[k]))
-    }
-    // match_rules.push(Reg(tpl[i].replace))
-    if (rules[i]) {
-      indexes.push(match_rules.length)
-      match_rules.push(_(rules[i]))
-    }
-  }
-
+export function Forward<T>(rulefn: () => Rule<T>) {
   return new Rule((input, pos, skip) => {
-    var res = [] as any[] // just being lazy
-
-    for (var i = 0, l = match_rules.length; i < l; i++) {
-      var mr = match_rules[i]
-      var match = mr.parse(input, pos, skip)
-      if (match === NoMatch) return NoMatch
-      if (indexes[i]) res.push(match.res)
-      pos = match.pos
-    }
-
-    if (res.length === 0) return Res(null, pos)
-    if (res.length === 1) return Res(res[0], pos)
-    return Res(res, pos)
+    return rulefn().parse(input, pos, skip)
   })
 }
 
 
-export const SeparatedBy = <T extends RawRule<any>>(rule: T, sep: RawRule<any>): Rule<Result<T>[]> =>
-Seq({
-  first: rule,
-  others: Repeat(Seq({ sep, rule }).map(r => r.rule))
-}).map(r => [r.first, ...r.others])
+export function Parser<T>(rule: Rule<T>, skip?: RegExp) {
+  skip = skip ? new RegExp(skip.source, skip.flags + 'y') : undefined
+  return function (input: string) {
+    Res.max_res = null
+    var res = rule.parse(input, 0, skip)
+    if (res === NoMatch) return NoMatch
+    var pos = res.pos
+    if (skip) {
+      var msk: RegExpMatchArray | null
+      while ((skip.lastIndex = pos, msk = skip.exec(input))) {
+        pos += msk[0].length
+      }
+    }
+    if (pos !== input.length) return NoMatch
+    // I should check if there is a skip
+    return res
+  }
+}
+
+
+export function SeparatedBy<T>(rule: Rule<T>, sep: RawRule<any>): Rule<T[]> {
+  return Seq({
+    first: rule,
+    others: Repeat(Seq(sep, { rule }).map(r => r.rule))
+  }).map(r => [r.first, ...r.others])
+}

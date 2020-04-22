693da71d4d3abba3c076b0706a4132ae68f060ca
Christophe Eymard <christophe.eymard@sales-way.com>
2020-04-22 19:17:59 +0200

    testsuite for everyone

--- tests/json5.ts
+++ tests/json5.ts
@@ -3,26 +3,14 @@ import * as J5 from 'json5'
 
 console.log(parser.parse(test))
 
-import { Suite } from 'benchmark'
-import * as Benchmark from 'benchmark'
+import { benchmark, runBenchmarks } from './testsuite'
 
-var s = new Suite()
-
-s.add(function Json5() {
+benchmark('Original JSON5', function Json5() {
   J5.parse(test)
 })
 
-s.add(function Json5Parseur() {
+benchmark('Parseur json5', function Json5Parseur() {
   parser.parse(test)
 })
 
-
-s.run()
-
-var benches = s.run({maxTime: 1 }) as unknown as Benchmark[]
-for (var b = 0, l = benches.length; b < l; b++) {
-  var bench = benches[b]
-  var name = typeof bench.fn === 'function' ? bench.fn.name : bench.fn
-  console.log(name, Math.round(bench.hz))
-
-}
+runBenchmarks()
